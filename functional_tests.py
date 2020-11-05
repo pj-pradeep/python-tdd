9b66960d133668d42bd2e489674ece05f84ca94d
Pradeep Pyatalo Jayarama <pradeep.pyatalojayarama@wpengine.com>
2020-11-05 01:44:17 -0600

    Refactor check for list item text in the HTML table to a helper method

--- functional_tests.py
+++ functional_tests.py
@@ -46,9 +46,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
