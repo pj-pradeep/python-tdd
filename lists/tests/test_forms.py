092e215ccbede0b20eec1cf8a279dc2dcf674cfe
Pradeep Pyatalo Jayarama <pradeep.pyatalojayarama@wpengine.com>
2020-11-16 23:20:21 -0600

    use the form save method to add item to a list. Refactor views to use the form save method

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.models import Item, List
 
 class ItemFormTest(TestCase):
 
@@ -16,3 +17,12 @@ class ItemFormTest(TestCase):
             form.errors['text'],
             [EMPTY_ITEM_ERROR]
         )
+
+
+    def test_form_save_handles_saving_to_a_list(self):
+        new_list = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=new_list)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, new_list)
