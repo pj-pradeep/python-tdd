375ec049540e64c36c2badcc6bf2b679161b61db
Pradeep Pyatalo Jayarama <pradeep.pyatalojayarama@wpengine.com>
2020-11-19 15:44:38 -0600

    Extend item form to handle uniqueness validation

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPBLICATE_ITEM_ERROR,EMPTY_ITEM_ERROR,
+    ItemForm, ExistingListItemForm
+)
 from lists.models import Item, List
 
 class ItemFormTest(TestCase):
@@ -26,3 +29,26 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item, Item.objects.first())
         self.assertEqual(new_item.text, 'do me')
         self.assertEqual(new_item.list, new_list)
+
+
+class ExistingListItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        new_list = List.objects.create()
+        form = ExistingListItemForm(for_list=new_list)
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+
+
+    def test_form_validation_for_blank_items(self):
+        new_list = List.objects.create()
+        form = ExistingListItemForm(for_list=new_list, data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+
+    def test_form_validation_for_duplicate_items(self):
+        new_list = List.objects.create()
+        Item.objects.create(list=new_list, text='no twins!')
+        form = ExistingListItemForm(for_list=new_list, data={'text': 'no twins!'})
+        self.assertFalse(form.is_valid())
+        self.assertEquals(form.errors['text'], [DUPBLICATE_ITEM_ERROR])
