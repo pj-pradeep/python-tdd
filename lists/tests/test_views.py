cd84983126ee9729eddeffdd144ecb32eac99607
Pradeep Pyatalo Jayarama <pradeep.pyatalojayarama@wpengine.com>
2020-11-22 00:42:10 -0600

    add jquery, qunit tests, list.js wit keypress listeners

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,7 +4,10 @@ from django.test import TestCase
 from django.utils.html import escape
 
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm, ExistingListItemForm,
+    EMPTY_ITEM_ERROR, DUPBLICATE_ITEM_ERROR
+)
 
 
 class HomePageTest(TestCase):
@@ -53,7 +56,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         new_list = List.objects.create()
         response = self.client.get(f'/lists/{new_list.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 
@@ -78,14 +81,14 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
+
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -94,7 +97,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPBLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
